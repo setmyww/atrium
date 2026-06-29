061265ad097d76a5e29431c81bd62dc7eb2a6f07
setmyww <koban2609@gmail.com>
2026-06-29 19:28:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.get('/api/me', (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
